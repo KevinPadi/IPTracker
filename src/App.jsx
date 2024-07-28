a5046bee77938350ec54374f686992f35921d681
Kevin <kevpadilla01@gmail.com>
2024-07-28 18:35:08 -0300

    leaflet map added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import { Input } from './components/ui/input'
 import { Button } from './components/ui/button'
 import { Card } from './components/ui/card'
 import { IconGitFork, IconDeviceDesktopPin, IconMapPin, IconClockHour2, IconWorld, IconZoom } from '@tabler/icons-react';
-
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet'
+import blackPin from './assets/black_pin.svg'
 
 function App() {
+
+  const customIcon = new L.Icon({
+    iconUrl: blackPin,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  })
+  const center = [-34.8941, -56.1528];
   return (
-    <div className='w-screen h-screen bg-black'>
+    <div className='w-screen h-screen flex bg-black'>
       <section className='flex flex-col gap-10 h-screen w-1/3 p-8 border-r border-blue-500'>
         <div className='w-full mx-auto flex gap-3 items-center'>
           <Input className='bg-black' placeholder='Enter a IP or a Domain name' />
@@ -47,6 +59,15 @@ function App() {
           </div>
         </Card>
       </section>
+      <MapContainer center={center} zoom={13} className='size-full'>
+      <TileLayer
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        minZoom={0}
+        maxZoom={20}
+        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={center} icon={customIcon} />
+    </MapContainer>
     </div>
   )
 }
